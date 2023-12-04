4546e23e485abed0fa3c331c14538d5028cb0984
snymnd <yunusenvire@gmail.com>
2023-12-04 12:25:01 +0700

    feat: make code ready to be an executable

--- repo_listing.py
+++ repo_listing.py
@@ -1,5 +1,13 @@
 from webdriver import driver
 import pandas as pd
+from datetime import datetime
+import os
+import sys
+
+date = datetime.now()
+str_now = date.strftime("%Y-%m-%d")
+
+application_path = os.path.dirname(sys.executable)
 
 containers = driver.find_elements(
     by="xpath", value='//*[@id="user-repositories-list"]/ul/li'
@@ -15,5 +23,13 @@ for container in containers:
     result["title"].append(title)
     result["link"].append(link)
 
+file_name = f"result_{str_now}.csv"
+res_path = os.path.join(application_path, file_name)
 
-pd.DataFrame(result).to_csv("result.csv", index=False)
+pd.DataFrame(result).to_csv(res_path)
+
+driver.quit()
+
+## run with pyinstaller to make an executable (py to exe)
+#  pyinstaller --onefile repo_listing.py
+## then the executable will be in dist folder
--- webdriver.py
+++ webdriver.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
-import os
+options = Options()
 
-os_path = os.path.dirname(os.path.realpath(__file__))
+## Headless mode, making the browser invisible (run in background)
+options.add_argument("--headless=new")   
+
 
 website = "https://github.com/snymnd?tab=repositories"
-webdriver_path = os_path + "/chromedriver"
+# adjust your file directory here
+webdriver_path = "/home/snymnd/project/Automate/chromedriver"
+print(webdriver_path)
 
 service = Service(executable_path=webdriver_path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service, options=options)
 
 driver.get(website)
